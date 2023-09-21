a8af8db4e30a151adbaf8087a72f4ad1b0c1817b
Jia-Yi Jian <jyjian@illinois.edu>
2023-09-21 12:49:47 -0700

    add react-bootstrap container as top-section's div

--- src/components/top-section.js
+++ src/components/top-section.js
@@ -1,1 +1,12 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+
+const TopSection = () => {
+    return (
+        <Container fluid>
+            <Row>
+                <Col>AppetAIzing</Col>
+            </Row>
+        </Container>
+    )
+}
