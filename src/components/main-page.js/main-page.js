e45d3b5defb016473c57d095dcfb7215855fa4a3
Jia-Yi Jian <jyjian@illinois.edu>
2023-09-22 14:00:04 -0700

    added tyles and class name into main page

--- src/components/main-page.js/main-page.js
+++ src/components/main-page.js/main-page.js
@@ -1,13 +1,19 @@
 import React from "react";
 import TopSection from "../top-section/top-section";
 import LeftSection from "../left-section/left-section";
+import { Container, Row, Col } from "react-bootstrap";
 
 const MainPage = () => {
     return (
-        <div>
-            <TopSection />
-            <LeftSection/>
-        </div>
+        <Container fluid style={{height:"100%"}}>
+            <Row className="d-flex justify-content-center" fluid style={{backgroundColor: "blanchedalmond", height: "10%"}}>
+                <Col className="d-flex align-items-center justify-content-center"><TopSection /></Col>
+            </Row>
+            <Row>
+                <Col ><LeftSection /></Col>
+                <Col>text and button</Col>
+            </Row>
+        </Container>
     );
 }
 
--- src/components/top-section/top-section.js
+++ src/components/top-section/top-section.js
@@ -4,13 +4,14 @@ import "./top-section.styles.css";
 
 const TopSection = () => {
     return (
-        <Container className="d-flex justify-content-center" fluid style={{backgroundColor: "blanchedalmond", height: "10%"}}>
-            <Row className="d-flex">
-                <Col className="d-flex align-items-center">
-                <h1 className="d-flex">AppetAIzing</h1>
-                </Col>
-            </Row>
-        </Container>
+        // <Container className="d-flex justify-content-center" fluid style={{backgroundColor: "blanchedalmond", height: "10%"}}>
+        //     <Row className="d-flex">
+        //         <Col className="d-flex align-items-center">
+        //         <h1 className="d-flex">AppetAIzing</h1>
+        //         </Col>
+        //     </Row>
+        // </Container>
+        <h1>AppetAIzing</h1>
     )
 }
 
