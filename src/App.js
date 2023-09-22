0bfc7719b05bfc15259e9aee60c402a59c93d217
Jia-Yi Jian <jiayi.jian95@gmail.com>
2023-09-22 13:28:18 -0700

    created left-section component and added both top and left section to main page component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import SignUp from "../src/components/sign-up/sign-up.component.js";
-import TopSection from "./components/top-section/top-section.js";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import MainPage from "./components/main-page.js/main-page.js";
 
 const App = () => {
 
   return (
     <Router>
     <Routes>
-      <Route path="/" element={<TopSection/>}/>
+      <Route path="/" element={<MainPage/>}/>
       <Route path="/signup" element={<SignUp/>}/>
     </Routes>
   </Router>
---
+++ src/components/main-page.js/main-page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TopSection from "../top-section/top-section";
+import LeftSection from "../left-section/left-section";
+
+const MainPage = () => {
+    return (
+        <div>
+            <TopSection />
+            <LeftSection/>
+        </div>
+    );
+}
+
+export default MainPage;
