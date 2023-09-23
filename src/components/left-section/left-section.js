866c15d937c97c2f32a7306af460a2237f64920e
Jia-Yi Jian <jiayi.jian95@gmail.com>
2023-09-22 19:19:04 -0700

    created left section and added a pitcure to it

--- src/components/left-section/left-section.js
+++ src/components/left-section/left-section.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Image from 'react-bootstrap/Image';
 
 const LeftSection = () => {
     return (
-        <img src="https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="Main Page Image" />
+            <Image src="https://thumbs.dreamstime.com/b/assorted-indian-recipes-food-various-spices-rice-wooden-table-92742528.jpg" alt="Main Page Image"/>
     )
 }
 
