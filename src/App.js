dc98282262884be3240e05b4154b3994ea9fc85a
Jia-Yi Jian <jyjian@illinois.edu>
2023-09-28 12:16:15 -0700

    added submit recipr url form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import SignUp from "../src/components/sign-up/sign-up.component.js";
 import TopSection from "./components/top-section/top-section.js";
+import AICreateRecipe from "./components/ai-create-recipe/ai-create-recipe.js";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<TopSection/>}/>
       <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/ai-create-recipe" element={<AICreateRecipe/>}/>
     </Routes>
   </Router>
   );
---
+++ src/components/ai-create-recipe/ai-create-recipe.js
@@ -0,0 +1,15 @@
+const AICreateRecipe = () => {
+
+    return (
+        <>
+        <label>
+            Summarize a Recipe by AI:
+            <input type="text" name="recipeURL" value="Recipe URL"/>
+        </label>
+        <button type="submit">Submit URL</button>
+
+        </>
+    )
+}
+
+export default AICreateRecipe;
