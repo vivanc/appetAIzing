3374eef88c8634d9f0ab269bb3cb7e2dba9f7f7b
Jia-Yi Jian <jyjian@illinois.edu>
2023-09-28 16:16:10 -0700

    combined ai create recipe and user create recipe components into create recipe component

---
+++ src/components/create-recipe/create-recipe.component.js
@@ -0,0 +1,12 @@
+import AICreateRecipe from "./ai-create-recipe.component";
+import UserCreateRecipe from "./user-create-recipe.coponent";
+
+const CreateRecipe = () => {
+    <>
+    <AICreateRecipe />
+    <h3>OR</h3>
+    <UserCreateRecipe />
+    </>
+}
+
+export default CreateRecipe;
