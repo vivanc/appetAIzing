7890c4ce2159030cbd6b783efc618b411c8ad917
Jia-Yi Jian <jyjian@illinois.edu>
2023-09-28 16:14:17 -0700

    added AI create recipe component and user create recipe component

---
+++ src/components/create-recipe/ai-create-recipe.component.js
@@ -0,0 +1,11 @@
+const AICreateRecipe = () => {
+    <>
+    <label>
+        Recipe URL: <input name="recipeURL"/>
+    </label>
+    <button type="submit">Submit URL</button>
+
+    </>
+}
+
+export default AICreateRecipe;
