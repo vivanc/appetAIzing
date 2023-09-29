00c95a927ed20382b129d74f258b93eae136ca5b
Jia-Yi Jian <jiayi.jian95@gmail.com>
2023-09-28 21:29:48 -0700

    added title to ai create recipe form

--- src/components/create-recipe/ai-create-recipe.component.js
+++ src/components/create-recipe/ai-create-recipe.component.js
@@ -2,14 +2,16 @@ const AICreateRecipe = () => {
     console.log("ai recipe is rendring");
     return (
         <>
-        <label>
-            Recipe URL: <input name="recipeURL"/>
-        </label>
-        <button type="submit">Submit URL</button>
-    
+            <form>
+                <h4>Create a Recipe by AI</h4>
+                <label>
+                    Recipe URL: <input name="recipeURL" />
+                </label>
+                <button type="submit">Submit URL</button>
+            </form>
         </>
     )
-   
+
 }
 
 export default AICreateRecipe;
