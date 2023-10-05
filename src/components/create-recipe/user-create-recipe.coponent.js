2dd2a3823a8a89b3c369188358fa19b152581092
Vivian Chang <cyintzu@gmail.comgit config --global user.name Vivian>
2023-10-05 15:51:23 -0700

    added view-recipe component to home-page, change home-page logic to view view-recipe or recipe-card

--- src/components/create-recipe/user-create-recipe.coponent.js
+++ src/components/create-recipe/user-create-recipe.coponent.js
@@ -1,45 +1,39 @@
 const UserCreateRecipe = () => {
-    return (
-        <>
-            <form>
-                <h4>Create a Recipe From Scratch</h4>
-                <div className="form-group">
-                    <div>
-                        <label>Recipe Name:</label>
-                    </div>
-                    <div>
-                        <textarea name="steps" rows={1} cols={50}></textarea>
-                    </div>
-                    <div>
-                        <label>Ingredients</label>
-                    </div>
-                    <div>
-                        <textarea name="ingredients" rows={8} cols={120}></textarea>
-                    </div>
-                    <div>
-                        <label>Steps</label>
-                    </div>
-                    <div>
-                        <textarea name="steps" rows={8} cols={120}></textarea>
-                    </div>
-                    <div>
-
-                    </div>
-                    <div>
-                        <label>Upload Picture</label>
-                    </div>
-                    <div>
-                        <input type="file" name="recipeImage"></input>
-                    </div>
-                    <button type="submit">Submit Recipe</button>
-                </div>
-            </form>
-
-
-
-        </>
-    )
-
-}
+  return (
+    <>
+      <form>
+        <h4>Create a Recipe From Scratch</h4>
+        <div className="form-group">
+          <div>
+            <label>Recipe Name:</label>
+          </div>
+          <div>
+            <textarea name="steps" rows={1} cols={50}></textarea>
+          </div>
+          <div>
+            <label>Ingredients</label>
+          </div>
+          <div>
+            <textarea name="ingredients" rows={8} cols={120}></textarea>
+          </div>
+          <div>
+            <label>Steps</label>
+          </div>
+          <div>
+            <textarea name="steps" rows={8} cols={120}></textarea>
+          </div>
+          <div></div>
+          <div>
+            <label>Upload Picture</label>
+          </div>
+          <div>
+            <input type="file" name="recipeImage"></input>
+          </div>
+          <button type="submit">Submit Recipe</button>
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default UserCreateRecipe;
