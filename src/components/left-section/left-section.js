92991e0ae96bbb918af9a66ccd4b59cca4933214
Jia-Yi Jian <jyjian@illinois.edu>
2023-10-06 14:41:05 -0700

    fixed responsive image of main page left section

--- src/components/left-section/left-section.js
+++ src/components/left-section/left-section.js
@@ -3,7 +3,7 @@ import Image from 'react-bootstrap/Image';
 
 const LeftSection = () => {
     return (
-            <Image src="https://thumbs.dreamstime.com/b/assorted-indian-recipes-food-various-spices-rice-wooden-table-92742528.jpg" alt="Main Page Image"/>
+            <Image style={{width:"100%", height:"100%", objectFit: "cover"}} src="https://thumbs.dreamstime.com/b/assorted-indian-recipes-food-various-spices-rice-wooden-table-92742528.jpg" alt="Main Page Image"/>
     )
 }
 
