1174a4593a3277e8de8753c5164babdd680d1f70
Jia-Yi Jian <jiayi.jian95@gmail.com>
2023-10-06 21:10:42 -0700

    added link to signup button

--- src/components/intro-right-section/intro-right-section.component.js
+++ src/components/intro-right-section/intro-right-section.component.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Bootstrap from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const IntroRightSection = () => {
+
     return (
         <div >
             <div className="text-center">
@@ -9,8 +11,12 @@ const IntroRightSection = () => {
                 AppetAIzing is your culinary companion, a platform designed for recipe enthusiasts. 
                 Simply create a recipe with our AI-powered feature!
             </h4>
-            <button>Login</button>
+            {/* <Link to='/signin'>
+            <button>Signin</button>
+            </Link> */}
+            <Link to='/signup'>
             <button>Signup</button>
+            </Link>
             </div>
            
         </div>
