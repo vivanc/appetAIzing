2e2767770ed0b2ce4d8a6e9dd86800b91361344c
Jia-Yi Jian <jyjian@illinois.edu>
2023-10-06 22:10:34 -0700

    added related components to create-recipe component

--- src/components/create-recipe/create-recipe.component.js
+++ src/components/create-recipe/create-recipe.component.js
@@ -1,16 +1,33 @@
 import AICreateRecipe from "./ai-create-recipe.component";
 import UserCreateRecipe from "./user-create-recipe.coponent";
+import TopSection from "../top-section/top-section.component";
+import IntroLeftSection from "../intro-left-section/intro-left-section.component";
+import { Container, Row, Col } from "react-bootstrap";
 
 const CreateRecipe = () => {
     console.log("create recipe page is rendering");
     return (
         <>
-    <AICreateRecipe />
-    <h5>OR</h5>
-    <UserCreateRecipe />
-    </>
+            <Container>
+                <Row style={{ height: "10vh", width: "100vw" }}>
+                    <Col ><TopSection /></Col>
+                </Row>
+                <Row style={{ height: "90vh", width: "100vw" }}>
+                    <Col ><IntroLeftSection /></Col>
+                    <Col >
+                        <div>
+                            <AICreateRecipe />
+                            <h5>OR</h5>
+                            <UserCreateRecipe />
+                        </div>
+
+                    </Col>
+                </Row>
+            </Container>
+
+        </>
     )
-    
+
 }
 
 export default CreateRecipe;
