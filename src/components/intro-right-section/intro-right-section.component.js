41913936e6d8d17b01d523c1546b634bd144aa1a
Jia-Yi Jian <jyjian@illinois.edu>
2023-10-06 19:14:15 -0700

    wrapped main-page container inside a div for a better layout

---
+++ src/components/intro-right-section/intro-right-section.component.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Bootstrap from "react-bootstrap";
+
+const IntroRightSection = () => {
+    return (
+        <div >
+            <div className="text-center">
+            <h4>
+                AppetAIzing is your culinary companion, a platform designed for recipe enthusiasts. 
+                Simply create a recipe with our AI-powered feature!
+            </h4>
+            <button>Login</button>
+            <button>Signup</button>
+            </div>
+           
+        </div>
+    )
+}
+
+export default IntroRightSection;
