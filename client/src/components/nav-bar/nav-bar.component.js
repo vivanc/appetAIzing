249b895694832e865c75de357a25dbef9339e54f
Vivian Chang <cyintzu@gmail.comgit config --global user.name Vivian>
2023-10-26 12:29:33 -0700

    added links to nav-bar

--- client/src/components/nav-bar/nav-bar.component.js
+++ client/src/components/nav-bar/nav-bar.component.js
@@ -1,16 +1,29 @@
 import { ReactComponent as ProfileLogo } from "../../assets/profile.svg";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav>
-      <div className="pb-3">
-        <ProfileLogo />
-      </div>
-      <ul className="list-unstyled h3">
-        <li className="py-3 my-3 bg-secondary">Home</li>
-        <li className="py-3 my-3 bg-secondary">Add Recipe</li>
-        <li className="py-3 my-3 bg-secondary">Log Out</li>
-      </ul>
+    <nav className="d-flex flex-column ">
+      <ProfileLogo className="mx-auto" />
+
+      <Link
+        className="py-3 my-3 bg-secondary text-white h4 text-decoration-none"
+        to="/home"
+      >
+        Home
+      </Link>
+      <Link
+        className="py-3 my-3 bg-secondary text-white h4 text-decoration-none"
+        to="/create-recipe"
+      >
+        Add Recipe
+      </Link>
+      <Link
+        className="py-3 my-3 bg-secondary text-white h4 text-decoration-none"
+        to=""
+      >
+        Log Out
+      </Link>
     </nav>
   );
 };
