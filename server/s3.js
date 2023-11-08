93be01ae417899a11683ca07194dc46d929ad00d
Vivian Chang <cyintzu@gmail.comgit config --global user.name Vivian>
2023-11-07 16:35:25 -0800

    added corresponding server code to display images from s3

--- server/s3.js
+++ server/s3.js
@@ -1,15 +1,15 @@
 require("dotenv").config({ path: `.env.development` });
 // const aws = require("aws-sdk");
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const {
+  S3Client,
+  PutObjectCommand,
+  ListObjectsV2Command,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
+
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 const uuid = require("uuid");
-
-// aws.config.update({
-//   region: process.env.AWS_REGION,
-//   accessKeyID: process.env.AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-// });
-
 const s3Client = new S3Client({ region: process.env.AWS_REGION });
 const userID = "123";
 
@@ -31,4 +31,38 @@ const uploadToS3 = async ({ file, userID }) => {
   }
 };
 
-module.exports = { uploadToS3 };
+// get all object keys
+const getImageKeysByUser = async (userID) => {
+  const command = new ListObjectsV2Command({
+    Bucket: process.env.AWS_BUCKET,
+    Prefix: userID,
+  });
+
+  // ListObjectsV2Command returns an array of properties and we want Contents property
+  const { Contents = [] } = await s3Client.send(command);
+
+  return Contents.map((image) => image.Key);
+};
+
+// create presigned url for all keys
+const getUserPresignedUrls = async (userID) => {
+  try {
+    const imageKeys = await getImageKeysByUser(userID); // = [Keys, Keys, Keys]
+
+    const presignedUrls = await Promise.all(
+      imageKeys.map((key) => {
+        const command = new GetObjectCommand({
+          Bucket: process.env.AWS_BUCKET,
+          Key: key,
+        });
+        return getSignedUrl(s3Client, command, { expiresIn: 900 });
+      })
+    );
+    return { presignedUrls };
+  } catch (error) {
+    console.log("👎");
+    return { error };
+  }
+};
+
+module.exports = { uploadToS3, getUserPresignedUrls };
