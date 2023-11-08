ffe80863b7327f19a3b7f97fe5c64887a230b59c
Jia-Yi Jian <jiayi.jian95@gmail.com>
2023-11-08 14:51:09 -0800

    edited update view recipe component and left some errors to be sloved in the edit function

--- client/src/components/view-recipe/view-recipe.component.js
+++ client/src/components/view-recipe/view-recipe.component.js
@@ -4,27 +4,40 @@ import { useState, useEffect } from "react";
 import "./view-recipe.styles.css";
 
 const ViewRecipe = () => {
-  const [recipe, setRecipe] = useState({});
+  const [recipe, setRecipe] = useState({ingredients: [], steps: []});
   //switch between view mode and edit mode using editing hook
-  const [isEditing, setIsEditing] = useState(true);
+  const [isEditing, setIsEditing] = useState(false);
   let { recipeId } = useParams();
+  // const [editedRecipe, setEditedRecipe] = useState({ingredients: [], steps: []})
+  
+
+  const switchEditMode = () => {
+    setIsEditing(!isEditing)
+  }
+
+  // const updateContent = (event) => {
+  //   setRecipe({...recipe, [event.target.name]: event.target.value })
+  // }
+
+  const saveEditedRecipe = (event) => {
+    setRecipe({...recipe, [event.target.name]: event.target.value })
+    axios.put(`/api/recipe/${recipeId}`, recipe)
+    .then((response) => {
+      console.log(response.data)
+      setIsEditing(!isEditing)
+    })
+  }
 
   useEffect(() => {
+    console.log(isEditing)
     axios
-      .get(`http://localhost:5001/api/recipe/${recipeId}`)
-      .then((response) => {
-        const returnedRecipe = response.data;
-        setRecipe(returnedRecipe[0]);
-      });
-  }, []);
+    .get(`http://localhost:5001/api/recipe/${recipeId}`)
+    .then((response) => {
+      const returnedRecipe = response.data;
+      setRecipe(returnedRecipe[0]);
+    })
+  }, [isEditing])
 
-  //pre-process recipe.steps and recipe.ingredients line by line and put within input value
-  // const convertInput = (elements) => {
-  //   let resultStr = "";
-  //   elements.map((element) => {
-  //     resultStr.concat(element).
-  //   })
-  // }
 
   return (
 
@@ -37,65 +50,70 @@ const ViewRecipe = () => {
       <div className="d-inline-flex justify-content-end">
         {
           isEditing === false ?
-            <button className="btn btn-light" type="button">
+            <button className="btn btn-light" onClick={switchEditMode}>
               Edit Recipe
             </button>
             :
-            <button className="btn btn-light" type="button">
+            <button form="editRecipeForm" className="btn btn-light" onClick={saveEditedRecipe}>
               Save Recipe
             </button>
         }
 
       </div>
 
+
       <div className="card-body">
         {
+
           isEditing === false ?
             <>
               <h3 className="card-title">{recipe.name}</h3>
               <br />
               <h5 className="card-title">Ingredients: </h5>
               <p className="card-text">
-              <ul>
-            {
-              recipe.ingredients.map((ingredient, index) => {
-                  return <li>{ingredient}</li>
-              })
-            }
-            </ul>
-                </p>
+                <ul>
+                  {
+                    recipe.ingredients.map((ingredient) => {
+                      return <li>{ingredient}</li>
+                    })
+                  }
+                </ul>
+              </p>
               <br />
               <h5 className="card-title">Steps: </h5>
               <p className="card-text">
-              <ul>
-              {
-                recipe.steps.map((step, index) => {
-                  return <li>{step}</li>
-                })
-              }
-            </ul>
-                </p>
+                <ul>
+                  {
+                    recipe.steps.map((step) => {
+                      return <li>{step}</li>
+                    })
+                  }
+                </ul>
+              </p>
             </>
             :
-            
-              <form>
-                <h3 className="card-title"><input value={recipe.name}></input></h3>
-                <br />
-                <h5 className="card-title">Ingredients: </h5>
-                <div><textarea className="input-style" 
-                value={recipe.ingredients}>
-                  </textarea>
-                  </div>
-                <br />
-                <h5 className="card-title">Steps: </h5>
-                <div><textarea type="text" class="form-control" value={recipe.steps}></textarea></div>
-                </form>
-             
+
+            <form id="editRecipeForm">
+              <h3 className="card-title">
+                <input type='text' name="name" value={recipe.name}></input>
+              </h3>
+              <br />
+              <h5 className="card-title">Ingredients: </h5>
+              <div>
+              <textarea type='text' className="input-style" name="ingredients" value={recipe.ingredients.join('\n')}></textarea>
+              </div>
+              <br />
+              <h5 className="card-title">Steps: </h5>
+              <div>
+                <textarea type="text" name="steps" class="form-control" value={recipe.steps.join('\n')}></textarea>
+                </div>
+            </form>
+
         }
 
       </div>
     </div>
-  
-      );
+
+  );
 };
-      export default ViewRecipe;
+export default ViewRecipe;
