f1cbf170cdd1b6814663263474bc291d1e624d7d
Vivian Chang <cyintzu@gmail.comgit config --global user.name Vivian>
2023-11-08 14:17:34 -0800

    added filter states

--- client/src/components/recipe/recipe.component.js
+++ client/src/components/recipe/recipe.component.js
@@ -1,21 +1,32 @@
 import RecipeCard from "../recipe-card/recipe-card.component";
 import SearchBar from "../search-bar/search-bar.component";
+import { useState, useEffect } from "react";
 
-const Recipe = (props) => {
-  const { recipes } = props;
+const Recipe = ({ recipes }) => {
+  const [searchField, setSearchField] = useState("");
+  const [filteredRecipes, setFilteredRecipes] = useState(recipes);
+
+  const handleSearch = (e) => {
+    setSearchField(e.target.value.toLowerCase());
+  };
+
+  useEffect(() => {
+    const newFilteredRecipes = recipes.filter((recipe) => {
+      return recipe.name.toLowerCase().includes(searchField);
+    });
+    setFilteredRecipes(newFilteredRecipes);
+  }, [recipes, searchField]);
 
   return (
     <>
       <div className="d-flex justify-content-center mt-3">
-        <SearchBar />
+        <SearchBar onChagneHandler={handleSearch} />
       </div>
       <div className="row row-cols-1 row-cols-md-4 justify-content-center">
         {recipes.map((recipe) => {
-          return <RecipeCard recipe={recipe} />;
+          return <RecipeCard recipe={filteredRecipes} />;
         })}
-        {/* className="d-flex col-md-12" */}
       </div>
-
     </>
   );
 };
--- client/src/components/search-bar/search-bar.component.js
+++ client/src/components/search-bar/search-bar.component.js
@@ -1,15 +1,15 @@
-const SearchBar = () => {
+const SearchBar = ({ onChangeHandler }) => {
   return (
     <div className="d-flex flex-row rounded">
       <input
         className="form-control rounded"
         type="search"
         placeholder="search recipe.."
-        onChange={""}
+        onChange={onChangeHandler}
       />
-      <button className="btn btn-outline-warning my-2 my-sm-0" type="submit">
+      {/* <button className="btn btn-outline-warning my-2 my-sm-0" type="submit">
         search
-      </button>
+      </button> */}
     </div>
   );
 };
